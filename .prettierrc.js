d1ee5e9bd25dbedab1243a3088291b77384b6303
Hamed Nami <hamed.n@smartx.ir>
2024-08-24 13:44:23 +0330

    config prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports= {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80
+}
