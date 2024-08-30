928cc1539b4fda5f6f0aaedd282a7a13133b2902
hamed.nm21 <hamed.nami1994@gmail.com>
2024-08-31 00:31:53 +0330

    using classes package

--- src/components/Common/RadioGroup.tsx
+++ src/components/Common/RadioGroup.tsx
@@ -1,19 +1,18 @@
 import React, {useState} from 'react';
 import {RadioButtonOptionType} from '@/types/common.type';
+import classNames from "classnames";
 
 interface RadioGroupType {
   options: RadioButtonOptionType[];
   defaultCheckedRadio: RadioButtonOptionType;
   onChange: (value: RadioButtonOptionType) => void;
   layout?: 'horizontal' | 'vertical';
-  // disabled?: boolean;
 }
 
 const RadioGroup: React.FC<RadioGroupType> = ({
   options,
   defaultCheckedRadio,
   layout = 'horizontal',
-  // disabled,
   onChange
 }) => {
   const [checkedValue, setCheckedValue] = useState(defaultCheckedRadio);
@@ -32,28 +31,16 @@ const RadioGroup: React.FC<RadioGroupType> = ({
             items-center
         `}>
           <div
-            className={`
-              w-4 h-4 
-              cursor-pointer
-              border 
-              ${option.value === checkedValue.value ? 'border-gray-dark' : 'border-gray-normal'} 
-              rounded-full
-              mx-2
-              flex
-              justify-center
-              items-center
-              hover:border-gray-dark 
-              after:absolute
-              after:contents[""]
-              after:rounded-full
-              after:w-2
-              after:h-2
-              after:${option.value === checkedValue.value ? 'bg-primary-normal' : 'bg-transparent'}
-              after:transition-all
-              after:duration-200
-              transition-all
-              duration-200
-              `}
+            className={classNames(
+                'w-4 h-4 cursor-pointer border rounded-full mx-2 flex justify-center items-center hover:border-gray-dark relative transition-all duration-200',
+                {
+                  'border-gray-dark': option.value === checkedValue.value,
+                  'border-gray-normal': option.value !== checkedValue.value,
+                  'after:bg-primary-normal': option.value === checkedValue.value,
+                  'after:bg-transparent': option.value !== checkedValue.value,
+                },
+                'after:absolute after:rounded-full after:w-2 after:h-2 after:transition-all after:duration-200'
+            )}
           />
           <input
             className='relative hidden'
